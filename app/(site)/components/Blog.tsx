fa4d6f3957f4c147e36f07ce9ead6b6bbe482525
SHUBH0924 <shubhjeetpaul@gmail.com>
2023-11-30 02:03:21 +0530

    new home page error solved

--- app/(site)/components/Blog.tsx
+++ app/(site)/components/Blog.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { getBlogs } from "@/sanity/sanity.query";
 import type { BlogType } from "@/types";
+import Link from "next/link";
 
 export default async function Blog() {
   const blog: BlogType[] = await getBlogs();
@@ -17,8 +18,8 @@ export default async function Blog() {
             key={data._id}
             className="flex items-start lg:gap-x-6 gap-x-4 max-w-2xl relative before:absolute before:bottom-0 before:top-[4.5rem] before:left-7 before:w-[1px] before:h-[calc(100%-50px)] before:bg-zinc-800"
           >
-            <a
-              href={data.slug}
+            <Link
+              href={`/blogs/${data.slug}`}
               rel="noreferrer noopener"
               className="min-h-[60px] min-w-[60px] rounded-md overflow-clip relative"
             >
@@ -28,7 +29,7 @@ export default async function Blog() {
                 alt={`${data.title} logo`}
                 fill
               />
-            </a>
+            </Link>
             <div className="flex flex-col items-start">
               <h3 className="text-xl font-bold">{data.title}</h3>
               <p>{data.metadesc}</p>
